9f110aa475061ac25a99d76d8e51b39093f905ed
Yannick Matimbe <yannickrafael286@gmail.com>
2025-04-03 12:15:11 +0200

    initial commit

---
+++ const.py
@@ -0,0 +1,16 @@
+courses = [
+    "lca",
+    "lea",
+    "lect",
+    "lecc",
+    "lemec",
+    "leet",
+    "lee",
+    "lef",
+    "leit",
+    "lemt",
+    "lgbs",
+    "lgf"
+]
+
+partial_link = 'https://fenix.isutc.ac.mz/isutc/cursos/'
---
+++ subject.py
@@ -0,0 +1,5 @@
+class Subject:
+    def __init__(self, name, course, identifier):
+        self.name = name
+        self.course = course
+        self.id = identifier
