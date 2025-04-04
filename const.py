02f373c83caaf51e85257ac1e52e52c7ecda5250
Yannick Matimbe <yannickrafael286@gmail.com>
2025-04-04 08:01:17 +0200

    created constant list of courses and base url

--- const.py
+++ const.py
@@ -1,16 +1,16 @@
-courses = [
-    "lca",
-    "lea",
-    "lect",
-    "lecc",
-    "lemec",
-    "leet",
-    "lee",
-    "lef",
-    "leit",
-    "lemt",
-    "lgbs",
-    "lgf"
-]
+courses = {
+    "lca": "QPqrE1GX6QMf5qbApL3Z",
+    "lea": "RapyWFeUyLIHeE3SaNoi",
+    "lect": "UpWqD3Pk0mtKRXQifHy2",
+    "lecc": "HK19qs7HEfS24S0fkLaA",
+    "lemec": "nDYtlNyw8VlyVbc3RNzx",
+    "leet": "h3Q0QlemC4TWMuRJiU2x",
+    "lee": "7j1XFXSStv3V2DWtYUtg",
+    "lef": "WeBe0mtllN4Q33BnqddJ",
+    "leit": "6dPFTslLTDbLLpRodc62",
+    "lemt": "SQHb4uT4uJwxfGdhJnQt",
+    "lgbs": "CuJoWLg0OcOxxBSswwfX",
+    "lgf": "bXlR5nXf5Wu7iHytAWhP"
+}
 
-partial_link = 'https://fenix.isutc.ac.mz/isutc/cursos/'
+partial_url = 'https://fenix.isutc.ac.mz/isutc/cursos/'
