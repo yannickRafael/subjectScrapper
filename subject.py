f3e314cb5523e884142be7f7dc6a33db07103152
Yannick Matimbe <yannickrafael286@gmail.com>
2025-04-04 08:00:40 +0200

    created parcelable subject class

--- subject.py
+++ subject.py
@@ -1,5 +1,17 @@
-class Subject:
-    def __init__(self, name, course, identifier):
+import json
+
+from cachecontrol.serialize import Serializer
+
+
+
+class Subject :
+    def __init__(self, name, identifier, ref):
         self.name = name
-        self.course = course
-        self.id = identifier
+        self.identifier = identifier
+        self.ref = ref
+
+
+class SubjectEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Subject):
+            return {"nome": obj.name, "codigo": obj.identifier, "cursoId": obj.ref}
